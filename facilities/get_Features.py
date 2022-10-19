b9a5f3f15b43e5af346b9643826558cf8af29d46
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2022-10-19 15:42:36 +0200

    Create get_Features.py

---
+++ facilities/get_Features.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+import json
+
+url = "https://search.k8s.srz.els.loc/api/geo/v2/institutions/getData"
+
+payload={}
+headers = {
+  'Cookie': 'ingresscookie-rescad=1666089565.541.25629.727648',
+  'Content-Type': 'application/json',
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+pretty_json = json.loads(response.text)
+print (json.dumps(pretty_json, indent=2))
+
+with open('facilities/facilities2.json', 'w', encoding='utf-8') as outfile:
+    json.dump(pretty_json, outfile, ensure_ascii=False, indent=4)
