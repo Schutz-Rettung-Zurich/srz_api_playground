e7afefad74a84e0844f61a648c66da5d04e69a3d
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2022-10-20 15:05:48 +0200

    insert stationboard

--- traffic_train/stationboard.py
+++ traffic_train/stationboard.py
@@ -12,5 +12,5 @@ response = requests.request("GET", url, headers=headers, data=payload)
 pretty_json = json.loads(response.text)
 print (json.dumps(pretty_json, indent=4))
 
-with open('stationboard.json', 'w', encoding='utf-8') as outfile:
+with open('traffic_train/stationboard.json', 'w', encoding='utf-8') as outfile:
     json.dump(pretty_json, outfile, ensure_ascii=False, indent=4)
